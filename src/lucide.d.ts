4428ac39256a27270e0399d119a5fe4dad76b944
sudhanv09 <mystogun125@gmail.com>
2024-11-12 14:59:09 +0800

    fix lucide importing all icons

---
+++ src/lucide.d.ts
@@ -0,0 +1,7 @@
+declare module "lucide-solid/icons/*" {
+    import { LucideProps } from "lucide-solid";
+    import { Component } from "solid-js";
+    const cmp: Component<LucideProps>;
+  
+    export = cmp;
+  }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import path from 'path';
+import { fileURLToPath } from "node:url"
 
 export default defineConfig({
   plugins: [solid()],
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src')
+      '~': path.resolve(__dirname, './src'),
+      "lucide-solid/icons": fileURLToPath(
+        new URL(
+          "./node_modules/lucide-solid/dist/source/icons",
+          import.meta.url,
+        ),
+      ),
     }
   }
 })
