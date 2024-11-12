732eabcf7a2a3b330a020eeab3bab55c2a0c0065
sudhanv09 <mystogun125@gmail.com>
2024-11-12 14:58:39 +0800

    implement add more fields button to modal

--- src/components/AddMemberModal.tsx
+++ src/components/AddMemberModal.tsx
@@ -13,9 +13,29 @@ import {
   TextFieldInput,
   TextFieldLabel,
 } from "~/components/ui/text-field";
-import { Plus } from "lucide-solid";
+import Plus from "lucide-solid/icons/plus";
+import Trash from "lucide-solid/icons/trash";
+import { createSignal, For, Show } from "solid-js";
+import { useParams } from "@solidjs/router";
 
 export function AddMemberModal() {
+  const [member, setMember] = createSignal([{ name: "", email: "" }]);
+  const roomId = useParams().id;
+
+  const addField = () => {
+    const newMember = {
+      name: "",
+      email: "",
+    };
+    setMember([...member(), newMember]);
+  };
+
+  const removeField = (idx: number) => {
+    let val = [...member()]
+    val.splice(idx, 1)
+    setMember(val)
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -24,25 +44,47 @@ export function AddMemberModal() {
       <DialogContent class="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Add Members</DialogTitle>
-          <DialogDescription>
-            Add participants to your group
-          </DialogDescription>
+          <DialogDescription>Add participants to your group</DialogDescription>
         </DialogHeader>
-        <div class="grid gap-4 py-4">
-          <TextField class="grid grid-cols-4 items-center gap-4">
-            <TextFieldLabel class="text-right">Name</TextFieldLabel>
-            <TextFieldInput
-              value="Pedro Duarte"
-              class="col-span-3"
-              type="text"
-            />
-          </TextField>
-          <TextField class="grid grid-cols-4 items-center gap-4">
-            <TextFieldLabel class="text-right">Username</TextFieldLabel>
-            <TextFieldInput value="@peduarte" class="col-span-3" type="text" />
-          </TextField>
-        </div>
+        <For each={member()}>
+          {(item, index) => (
+            <div class="flex gap-6">
+              <div class="grid gap-4 py-4">
+                <TextField class="grid grid-cols-4 items-center gap-4">
+                  <TextFieldLabel class="text-right">Name</TextFieldLabel>
+                  <TextFieldInput
+                    value={item.name}
+                    class="col-span-3"
+                    type="text"
+                  />
+                </TextField>
+                <TextField class="grid grid-cols-4 items-center gap-4">
+                  <TextFieldLabel class="text-right">Email</TextFieldLabel>
+                  <TextFieldInput
+                    value={item.email}
+                    placeholder="Optional"
+                    class="col-span-3"
+                    type="email"
+                  />
+                </TextField>
+              </div>
+              <Show when={member().length > 2}>
+                <Button
+                  variant="ghost"
+                  class="mt-4"
+                  onClick={() => removeField(index())}
+                >
+                  <Trash class="h-4" />
+                </Button>
+              </Show>
+            </div>
+          )}
+        </For>
+
         <DialogFooter>
+          <Button onClick={addField} type="submit">
+            Add more
+          </Button>
           <Button type="submit">Save changes</Button>
         </DialogFooter>
       </DialogContent>
